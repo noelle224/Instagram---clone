fb8826c3ea3906707a760d5dcabe774da7d12f8f
Bhavika Bharti <45075050+noelle224@users.noreply.github.com>
2022-04-02 13:12:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
-
+// write in your own firebase configurations.
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDcd5uwT5EE6BpN9P-pbO8tr8w6k0eVKpU",
-    authDomain: "instagram-clone-5dad7.firebaseapp.com",
-    projectId: "instagram-clone-5dad7",
-    storageBucket: "instagram-clone-5dad7.appspot.com",
-    messagingSenderId: "821551780776",
-    appId: "1:821551780776:web:cee867a3bd550e31bdc39d",
-    measurementId: "G-B51BKCWM0N"
+    apiKey: "//",
+    authDomain: "//",
+    projectId: "//",
+    storageBucket: "//",
+    messagingSenderId: "//",
+    appId: "1//",
+    measurementId: "//"
   });
   
   const db= firebaseApp.firestore();
